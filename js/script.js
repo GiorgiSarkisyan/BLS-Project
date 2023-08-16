23c6194390b3000c287c65a3974bf14380277d76
GiorgiSarkisyan <gios48320@gmail.com>
2023-08-16 23:16:26 +0400

    add bugfix-wav

--- js/script.js
+++ js/script.js
@@ -4,38 +4,44 @@ const users = [
     image: "GlobalImages/BLS_FRONTLINE-200x227.jpg",
     title: "BLS Frontline",
     alt: "ambulance",
+    href: "frontline-ambulance.html"
   },
   {
     image: "GlobalImages/BLS_MINIBUS-200x227.jpg",
     title: "Minibus",
     alt: "ambulance",
+    href: "minibus.html"
   },
   {
     image: "GlobalImages/BLS_PTS-200x227.jpg",
     title: "PTS",
     alt: "ambulance",
+    href: "#"
   },
   {
     image: "GlobalImages/BLS_SECURE-200x227.jpg",
     title: "Secure",
     alt: "ambulance",
+    href: "secure-transport.html"
   },
   {
     image: "GlobalImages/BLS_WAV-200x227.jpg",
     title: "WAV",
     alt: "ambulance",
+    href:"wav.html"
   },
   {
     image: "GlobalImages/BLS_HDU-200x227.jpg",
     title: "BLS HDU",
     alt: "ambulance",
+    href: "#"
   },
 ];
 
 for (let i = 0; i < users.length; i++) {
     cars.insertAdjacentHTML(
       "beforeend",
-      `<a href="frontline-ambulance.html" class="card">
+      `<a href="${users[i].href}" class="card">
       <div class ="card-image">
         <img src="${users[i].image}" alt="ambulance">
       </div>
